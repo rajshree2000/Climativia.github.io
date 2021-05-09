04b49bd855e15a18e86517f2da85690907f8542c
rajshree2000 <69753609+rajshree2000@users.noreply.github.com>
2021-05-09 18:34:27 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,48 @@
+var questions = [
+    {
+        title: "As global temperatures rise, areas in the Arctic could release even more greenhouse gas because:",
+        options: ["The frozen ground contains greenhouse gases like methane and carbon dioxide, and when the ground thaws, the gas would be released.", "Locals would burn their jackets to celebrate the milder winters, releasing greenhouse gases.", "The urge to do spring cleaning up north would be so great that the increase in vacuum use would use up a ton of energy, burning fossil fuels in the process.", "Bigfoot, a notoriously gassy species, would come out of hibernation."],
+        answer: "The frozen ground contains greenhouse gases like methane and carbon dioxide, and when the ground thaws, the gas would be released."
+    },
+
+    {
+        title: "Which of these weather events can be worsened by global warming?",
+        options: ["Hurricanes", "Raining cats and dogs", "Falling sky", "Flying Pigs"],
+        answer: "Hurricanes"
+    },
+
+    {
+        title: "Global warming is causing the ocean to get warmer, resulting in:",
+        options: ["More beach vacations.", "A reduced need to cook lobster before eating it.", "An increase in people mistaking the ocean for their own personal bathtub, leading to an increase in ocean soapiness.", "Coral reefs getting sick and losing their color."],
+        answer: "Coral reefs getting sick and losing their color."
+    },
+
+    {
+        title: "Fossil fuels, like the gas we put into our cars, comes from:",
+        options: ["Cavemen dinner leftovers.", "Organisms, mainly algae and other small life forms, that died and were buried millions of years in the past.", "Natural history museums.", "The internet."],
+        answer: "Organisms, mainly algae and other small life forms, that died and were buried millions of years in the past."
+    },
+
+    {
+        title: "Without the greenhouse effect:",
+        options: ["We would all have to live in actual greenhouses and become good friends with plants.", "There would be no life on Earth.", "We'd all be talking about the purplehouse effect.", "All of our homes would be painted brown."],
+        answer: "There would be no life on Earth."
+    },
+
+    {
+        title: "Which greenhouse gas is abundant in Earth's atmosphere?",
+        options: ["Carbon dioxide", "Cow gas", "Goat burps", "Water vapour"],
+        answer: "Water vapour"
+    },
+    {
+        title: "Why is it a good idea to turn off lights and other electronics when you are not using them?",
+        options: ["Because they might get bored after a long run of shining and beeping for nobody.", "Because all that energy comes from somewhere, and more likely than not it is coming from a power plant that is burning fossil fuels.", "Because your parents might think that you are playing videos games for a longer amount of time than you actually are. This could lead to less video game time.", "Because I told you so."],
+        answer: "Because all that energy comes from somewhere, and more likely than not it is coming from a power plant that is burning fossil fuels."
+    },
+    {
+        title: "Global warming causes sea level to rise because:",
+        options: ["All of these answers.", "It prevents ice and glaciers from forming, slowing down how much water is removed from the ocean each winter.", "It causes glaciers and ice caps to melt, putting more water into our oceans.", "Hotter things expand, and the warmer the ocean, the larger it gets."],
+        answer: "All of these answers."
+    },
+
+];
